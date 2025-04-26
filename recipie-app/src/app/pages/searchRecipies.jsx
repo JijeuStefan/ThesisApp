4a4d8cac74eed3d7f43de92844e215f4e2d871de
Stefan-code-eng1 <jijeus@gmail.com>
2025-04-26 21:06:05 +0300

    header
    added a better text font to the header

--- recipie-app/src/app/pages/searchRecipies.jsx
+++ recipie-app/src/app/pages/searchRecipies.jsx
@@ -5,7 +5,7 @@ export default function SearchRecipies(){
     return (
         <div className="flex flex-col">
           <Header />
-          <div className="">
+          <div className="font-inter">
             <h2>Welcome to Tailwind Docs Style</h2>
             <p>
             This is paragraph text styled like the Tailwind documentation.
